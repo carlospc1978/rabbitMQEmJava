f163cec8c9c6000b883436cdf5ab8624005f5c94
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-09-28 16:50:36 -0300

    primeiro commit

---
+++ src/main/java/br/com/seteideias/rabbitmqemjava/RabbitMqEmJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.rabbitmqemjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqEmJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqEmJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/seteideias/rabbitmqemjava/RabbitMqEmJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.rabbitmqemjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqEmJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
